9ec97d8d6401c1573f818e2b530fa274bbde10a5
ntnobara <nguyentoan231196@gmail.com>
2024-11-26 20:10:27 +0700

    write encode function

--- Base64_Enc_Dec/base64_basic.zig
+++ Base64_Enc_Dec/base64_basic.zig
@@ -58,3 +58,44 @@ const base64 = Base64.init();
 pub fn main() !void {
     std.debug.print("Character at index 28: {c}\n", .{base64.charAt(28)});
 }
+
+pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+    if (input.len == 0) return "";
+
+    const n_out = try calcEncodeLength(input);
+    var out = try allocator.alloc(u8, n_out);
+    var buf = [3]u8{ 0, 0, 0 };
+    var count: u8 = 0;
+    var iout: u64 = 0;
+
+    for (input, 0..) |_, i| {
+        buf[count] = input[i];
+        count += 1;
+
+        if (count == 3) {
+            out[iout] = self.charAt(buf[0] >> 2);
+            out[iout + 1] = self.charAt(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+            out[iout + 2] = self.charAt(((buf[1] & 0x0f) << 2) + (buf[2] >> 6));
+            out[iout + 3] = self.charAt(buf[2] & 0x3f);
+
+            iout += 4;
+            count = 0;
+        }
+    }
+
+    if (count == 1) {
+        out[iout] = self.charAt(buf[0] >> 2);
+        out[iout + 1] = self.charAt((buf[0] & 0x03) << 4);
+        out[iout + 2] = '=';
+        out[iout + 3] = '=';
+    }
+
+    if (count == 2) {
+        out[iout] = self.charAt(buf[0] >> 2);
+        out[iout + 1] = self.charAt(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+        out[iout + 2] = self.charAt((buf[1] & 0x0f) << 2);
+        out[iout + 3] = '=';
+        iout += 4;
+    }
+    return out;
+}
